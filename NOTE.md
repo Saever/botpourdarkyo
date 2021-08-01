1529ada5aeb1cc4c17c4cee400fea6ef7744c3e3
Saever <86686987+Saever@users.noreply.github.com>
2021-08-01 16:23:48 +0200

    Add files via upload

---
+++ NOTE.md
@@ -0,0 +1,19 @@
+```js
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports = {
+    name: 'nom de la commande', 
+    help: {
+        description: 'description de la commande',
+    },
+
+    async execute(client, message, args) {
+
+    }
+}
+
+//POUR VOIR ON SON LES ERREUR ALLER DANS TERMINAL POUR VOIR LES LIGNES OU SONT LES ERREURS//
+
+``` 
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const config = require('./config.json');
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+client.snipes = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+client.login(config.token);
